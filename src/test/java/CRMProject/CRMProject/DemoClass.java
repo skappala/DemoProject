c1af860b2de7568ab8e6293c28c3f56a3e2baf5e
skappala <saraswathi.kappala@gmail.com>
2020-10-07 13:01:37 -0400

    first program

---
+++ src/test/java/CRMProject/CRMProject/DemoClass.java
@@ -0,0 +1,13 @@
+package CRMProject.CRMProject;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
+
+	}
+
+}
